ad15647634e5dbba727f75f6c8f0d073044a02d7
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2023-04-16 02:45:33 +0700

    Cleaned up the code

--- sketch.js
+++ sketch.js
@@ -1,86 +1,86 @@
-const g = 0.098;
-const bouncyness = 0.9;
+const WIDTH = 640;
+const HEIGHT = 640;
+
+const FORCE_GRAVITY = 0.098;
+
+const COEFFICIENT_RESTITUTION = 0.9;
+
+function createBall() {
+    return {
+        size: Math.random() * 8 + 8,
+        x: Math.random() * WIDTH,
+        y: Math.random() * HEIGHT,
+        vx: Math.random() * 2 - 1,
+        vy: Math.random() * 2 - 1,
+        ax: 0,
+        ay: 0,
+    };
+}
 
 let n = 100;
-const balls = [];
+let balls = [];
 
 for (let i = 0; i < n; i++) {
-  balls.push({
-    x: Math.random() * 800,
-    y: Math.random() * 800,
-    vx: 0,
-    vy: 0,
-    ax: 0,
-    ay: 0,
-    size: Math.random() * 16 + 16,
-  })
+    balls.push(createBall());
 }
 
 function setup() {
-  createCanvas(800, 800);
+    createCanvas(WIDTH, HEIGHT);
 }
 
 function draw() {
-  background(24);
+    background(24);
+    fill(215);
+    textSize(16);
+    text('Click to apply force', 8, 24);
 
-  balls.forEach(ball => {
-    ball.vx += ball.ax;
-    ball.vy += ball.ay;
-    ball.ax = 0;
-    ball.ay = 0;
-    ball.x += ball.vx;
-    ball.y += ball.vy;
+    updatePosition();
 
-    if (ball.y < 0) {
-      ball.y = 0;
-      ball.vy = -ball.vy * bouncyness;
-    }
-
-    if (ball.y > 800) {
-      ball.y = 800;
-      ball.vy = -ball.vy * bouncyness;
-    }
-
-    if (ball.x < 0) {
-      ball.x = 0;
-      ball.vx = -ball.vx * bouncyness;
-    }
-
-    if (ball.x > 800) {
-      ball.x = 800;
-      ball.vx = -ball.vx * bouncyness;
-    }
-
-    ball.ay += g;
-
-    ellipse(ball.x, ball.y, ball.size, ball.size);
-  });
+    balls.forEach(ball => {
+        ellipse(ball.x, ball.y, ball.size, ball.size);
+    });
 }
 
 function mousePressed() {
-  balls.forEach(ball => {
-    fx = mouseX - ball.x;
-    fy = mouseY - ball.y;
+    balls.forEach(ball => {
+        let fx = (mouseX - ball.x) / WIDTH * 32 / ball.size;
+        let fy = (mouseY - ball.y) / HEIGHT * 32 / ball.size;
 
-    if (fx > 5) {
-      fx = 5;
-    }
-
-    if (fx < -5) {
-      fx = -5;
-    }
-
-    if (fy > 5) {
-      fy = 5;
-    }
-
-    if (fy < -5) {
-      fy = -5;
-    }
-
-    ball.ax += fx;
-    ball.ay += fy;
-  });
+        ball.ax += fx;
+        ball.ay += fy;
+    });
 }
 
+function updatePosition() {
+    balls.forEach(ball => {
+        const half_size = ball.size / 2;
 
+        ball.vx += ball.ax;
+        ball.vy += ball.ay;
+        ball.x += ball.vx;
+        ball.y += ball.vy;
+
+        if (ball.x > WIDTH - half_size) {
+            ball.x = WIDTH - half_size;
+            ball.vx = -ball.vx * COEFFICIENT_RESTITUTION;
+        }
+
+        if (ball.x < half_size) {
+            ball.x = half_size;
+            ball.vx = -ball.vx * COEFFICIENT_RESTITUTION;
+        }
+
+        if (ball.y > HEIGHT - half_size) {
+            ball.y = HEIGHT - half_size;
+            ball.vy = -ball.vy * COEFFICIENT_RESTITUTION;
+        }
+
+        if (ball.y < half_size) {
+            ball.y = half_size;
+            ball.vy = -ball.vy * COEFFICIENT_RESTITUTION;
+        }
+
+        ball.ax = 0;
+        ball.ay = FORCE_GRAVITY;
+    });
+}
